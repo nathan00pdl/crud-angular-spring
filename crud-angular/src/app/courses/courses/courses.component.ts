36aad7d1569bd2d9af48a36bcfb3cd2a4e14c56c
nathan00pdl <nathanpaiva00@gmail.com>
2024-12-06 19:42:16 -0300

    returning a list of courses by an http request

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Course } from '../model/course';
 import { AppMaterialModule } from '../shared-app-material/app-material-module';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -14,15 +15,13 @@ import { CoursesService } from '../services/courses.service';
 })
 
 export class CoursesComponent implements OnInit{
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
 
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
     this.courses = this.coursesService.list();
-  }
+}
 
-  ngOnInit(): void {
-    this.courses = this.coursesService.list();
-  }
+  ngOnInit(): void {}
 }
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,20 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
+import { first, take, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/crud-angular/public/courses.json'
+
   //Dependency Injection
-  constructor(private HttpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {
 
   }
 
-  list(): Course[]{
-    return [
-      { _id: '1', name: 'Angular', category: 'front-end' }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
