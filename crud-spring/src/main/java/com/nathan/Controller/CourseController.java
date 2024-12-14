2ccb58eb509bfbb6aab4d3f308a3f114026c7444
nathan00pdl <nathanpaiva00@gmail.com>
2024-12-14 19:41:57 -0300

    implementation of findById() method for searching courses only by indentifiers

--- crud-spring/src/main/java/com/nathan/Controller/CourseController.java
+++ crud-spring/src/main/java/com/nathan/Controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nathan.Model.Course;
 import com.nathan.Repository.CourseRepository;
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -31,6 +33,14 @@ public class CourseController {
     public List<Course> list(){
         return courseRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
+    }
+    
 
     @PostMapping
     public ResponseEntity<Course> create(@RequestBody Course course) {
