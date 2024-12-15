96f1fc3c39fc85aa845f47db21dd37870907e826
nathan00pdl <nathanpaiva00@gmail.com>
2024-12-15 09:33:42 -0300

    add a resolver service and configuring the edit button

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,8 +1,29 @@
 import { Routes } from '@angular/router';
+import { CourseResolver } from './courses/guards/course.resolver';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
-  { path: 'courses', loadComponent: () => import('./courses/containers/courses/courses.component').then(m => m.CoursesComponent) },
-  { path: 'courses/new', loadComponent: () => import('./courses/containers/course-form/course-form.component').then(m => m.CourseFormComponent) },
+  {
+    path: 'courses',
+    loadComponent: () =>
+      import('./courses/containers/courses/courses.component').then(
+        (m) => m.CoursesComponent
+      ),
+  },
+  {
+    path: 'courses/new',
+    loadComponent: () =>
+      import('./courses/containers/course-form/course-form.component').then(
+        (m) => m.CourseFormComponent
+      ),
+  },
+  {
+    path: 'courses/edit/:id',
+    loadComponent: () =>
+      import('./courses/containers/course-form/course-form.component').then(
+        (m) => m.CourseFormComponent
+      ),
+    resolve: { course: CourseResolver },
+  },
   { path: '**', redirectTo: '' },
 ];
