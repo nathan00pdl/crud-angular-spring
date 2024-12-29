f3ff2f1c99be2499476dff3f225fe2b39e85103d
nathan00pdl <nathanpaiva00@gmail.com>
2024-12-29 11:31:58 -0300

    validation annotations in the API model classes and DTOs

--- crud-spring/src/main/java/com/nathan/Model/Course.java
+++ crud-spring/src/main/java/com/nathan/Model/Course.java
@@ -20,7 +20,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 @Entity  //Course será uma entidade correspondente à uma tabela no BD
@@ -47,6 +49,9 @@ public class Course {
     @Convert(converter = StatusConverter.class)
     private Status status = Status.ACTIVE;
 
+    @NotNull
+    @NotEmpty
+    @Valid
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
     private List<Lesson> lessons = new ArrayList<>();
 
--- crud-spring/src/main/java/com/nathan/dto/LessonDTO.java
+++ crud-spring/src/main/java/com/nathan/dto/LessonDTO.java
@@ -1,11 +1,16 @@
 package com.nathan.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public record LessonDTO(
 
-        Long id,
+                Long id,
 
-        String name,
+                @NotNull @NotEmpty @Length(min = 5, max = 100) String name,
 
-        String youtubeUtl) {
+                @NotNull @NotEmpty @Length(min = 10, max = 11) String youtubeUtl) {
 
 }
