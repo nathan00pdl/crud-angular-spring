36ede9a824565d587bde4fdb84b1dd72e433b6f8
nathan00pdl <nathanpaiva00@gmail.com>
2024-12-29 15:44:36 -0300

    validation class for ENUM

--- crud-spring/src/main/java/com/nathan/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/nathan/dto/CourseDTO.java
@@ -1,23 +1,24 @@
 package com.nathan.dto;
 
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nathan.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
                 @JsonProperty("_id") Long id,
 
                 @NotBlank @NotNull @Length(min = 5, max = 100) String name,
 
-                @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
+                @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
 
                 @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
