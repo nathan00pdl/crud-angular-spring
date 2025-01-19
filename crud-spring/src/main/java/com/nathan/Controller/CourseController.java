35e5a5fef29ccab8a59b18a78e89fc5549fe591d
nathan00pdl <nathanpaiva00@gmail.com>
2025-01-19 16:45:31 -0300

    add searchByName and searchByStatus endpoints

--- crud-spring/src/main/java/com/nathan/Controller/CourseController.java
+++ crud-spring/src/main/java/com/nathan/Controller/CourseController.java
@@ -1,5 +1,8 @@
 package com.nathan.Controller;
 
+import java.io.ObjectInputFilter.Status;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +23,7 @@ import com.nathan.dto.CoursePageDTO;
 import io.micrometer.common.lang.NonNull;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -31,13 +35,15 @@ public class CourseController {
 
     private final CourseService courseService;
 
-    // Injeção de dependência via construtor (garante desacoplamento com a camada de serviço - princípio OCP do SOLID)
+    // Injeção de dependência via construtor (garante desacoplamento com a camada de
+    // serviço - princípio OCP do SOLID)
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
     }
 
     @GetMapping
-    public CoursePageDTO findAll(@RequestParam(defaultValue = "0") @PositiveOrZero int pageNumber,
+    public CoursePageDTO findAll(
+            @RequestParam(defaultValue = "0") @PositiveOrZero int pageNumber,
             @RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize) {
         return courseService.findAll(pageNumber, pageSize);
     }
@@ -45,6 +51,19 @@ public class CourseController {
     @GetMapping("/{id}")
     public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
         return courseService.findById(id);
+    }
+
+    @GetMapping("/searchByName")
+    public List<CourseDTO> findByName(@RequestParam @NotNull @NotBlank String name) {
+        return courseService.findByName(name);
+    }
+
+    @GetMapping("/searchByStatus")
+    public CoursePageDTO findByStatus(
+            @RequestParam @NotNull com.nathan.enums.Status status,
+            @RequestParam(defaultValue = "0") @PositiveOrZero int pageNumber,
+            @RequestParam(defaultValue = "10") @PositiveOrZero @Max(100) int pageSize) {
+        return courseService.findByStatus(status, pageNumber, pageSize);
     }
 
     @PostMapping
--- crud-spring/src/main/java/com/nathan/Service/CourseService.java
+++ crud-spring/src/main/java/com/nathan/Service/CourseService.java
@@ -1,7 +1,6 @@
 package com.nathan.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
