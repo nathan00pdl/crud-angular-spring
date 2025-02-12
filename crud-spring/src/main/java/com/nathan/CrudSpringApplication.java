fe285bba37ac597e6164cde5f58774d43db5b53c
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-12 19:35:02 -0300

    testing another request structure

--- crud-spring/src/main/java/com/nathan/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/nathan/CrudSpringApplication.java
@@ -22,22 +22,38 @@ public class CrudSpringApplication {
 		return args -> {
 			try {
 				courseRepository.deleteAll();
-				System.out.println("CURSOS DELETADOS COM SUCESSO!");
+				System.out.println("SUCCESSFULLY DELETED COURSES.");
 
-				for (int i = 1; i <= 10; i++) {
-					Course c = new Course();
-					c.setName("Curso " + i);
-					c.setCategory(i % 2 == 0 ? Category.BACK_END : Category.FRONT_END);
-	
-					Lesson l1 = new Lesson();
-					l1.setName("Aula introdutória: " + i);
-					l1.setYoutubeUrl("https://youtube.com/" + i);
-					l1.setCourse(c);
+				Course c1 = new Course();
+				c1.setName("Java COMPLETO + Orientação a Objetos");
+				c1.setCategory(Category.BACK_END);
 
-					c.getLessons().add(l1);
+				Lesson lesson1_c1 = new Lesson();
+				lesson1_c1.setName("Introdução a linguagem JAVA");
+				lesson1_c1.setYoutubeUrl("https://youtube.com/aula1");
+				lesson1_c1.setCourse(c1);
+				
+				Lesson lesson2_c1 = new Lesson();
+				lesson2_c1.setName("Orientação a objetos");
+				lesson2_c1.setYoutubeUrl("https://youtube.com/aula2");
+				lesson2_c1.setCourse(c1);
 
-					courseRepository.save(c);
-				}
+				c1.getLessons().add(lesson1_c1);
+				c1.getLessons().add(lesson2_c1);
+				courseRepository.save(c1);
+
+				Course c2 = new Course();
+				c2.setName("Angular framework");
+				c2.setCategory(Category.FRONT_END);
+
+				Lesson lesson1_c2 = new Lesson();
+				lesson1_c2.setName("Estrutura de diretórios com Angular");
+				lesson1_c2.setYoutubeUrl("https://youtube.com/aula1");
+				lesson1_c2.setCourse(c2);
+
+				c2.getLessons().add(lesson1_c2);
+				courseRepository.save(c2);
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
