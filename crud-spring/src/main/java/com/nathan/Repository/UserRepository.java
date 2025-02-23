791609554f36cc72293242a1a1f4bda68290422f
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-23 16:06:48 -0300

    add repository for user entity

---
+++ crud-spring/src/main/java/com/nathan/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.nathan.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import com.nathan.Model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
