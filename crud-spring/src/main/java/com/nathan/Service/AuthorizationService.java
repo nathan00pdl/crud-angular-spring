513e035915831f499ecea1821351dab63f18ece2
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-23 16:15:20 -0300

    add service for user authorization

---
+++ crud-spring/src/main/java/com/nathan/Service/AuthorizationService.java
@@ -0,0 +1,22 @@
+package com.nathan.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nathan.Repository.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService{
+    private UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
