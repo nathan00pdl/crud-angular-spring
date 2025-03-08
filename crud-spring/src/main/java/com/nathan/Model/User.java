09e3813ebaac514168d8d92c7a05db2f5bbf288b
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-08 17:58:36 -0300

    persisting the UserRole ENUM as a string

--- crud-spring/src/main/java/com/nathan/Model/User.java
+++ crud-spring/src/main/java/com/nathan/Model/User.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import com.nathan.enums.UserRole;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,6 +29,7 @@ public class User implements UserDetails {
     private String login;
     private String password;
 
+    @Enumerated(EnumType.STRING) // Hibernate Will persist the ENUM as a string and not as an ordinal
     private UserRole role;
 
     public User() {}
