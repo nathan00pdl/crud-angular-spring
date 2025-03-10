1db6cc59a4d540594e2453ca404e4fa9ce5dc9bf
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-10 19:56:33 -0300

    adding set methods for user attributes

--- crud-spring/src/main/java/com/nathan/Model/User.java
+++ crud-spring/src/main/java/com/nathan/Model/User.java
@@ -51,9 +51,17 @@ public class User implements UserDetails {
     public Long getId() {
         return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
     public String getLogin() {
         return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
     }
     
     public String getPassword() {
@@ -62,6 +70,10 @@ public class User implements UserDetails {
     
     public UserRole getRole() {
         return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
     
     @Override
