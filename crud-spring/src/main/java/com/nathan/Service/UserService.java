3c929ef9d835f1ab127c173a4c78f4b320ad7da5
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-11 08:07:51 -0300

    add service annotation to UserService

--- crud-spring/src/main/java/com/nathan/Service/UserService.java
+++ crud-spring/src/main/java/com/nathan/Service/UserService.java
@@ -2,10 +2,13 @@ package com.nathan.Service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.nathan.Repository.UserRepository;
 import com.nathan.dto.UserDTO;
 import com.nathan.dto.mapper.UserMapper;
 
+@Service
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
