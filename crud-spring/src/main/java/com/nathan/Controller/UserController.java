7504bf262d370746486a088dbfe9c28ab287beec
nathan00pdl <nathanpaiva00@gmail.com>
2025-03-11 08:42:11 -0300

    creating controller to return registred users

---
+++ crud-spring/src/main/java/com/nathan/Controller/UserController.java
@@ -0,0 +1,27 @@
+package com.nathan.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nathan.Service.UserService;
+import com.nathan.dto.UserDTO;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+}
